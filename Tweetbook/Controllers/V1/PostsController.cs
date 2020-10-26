9608c3ef597068964c5e3faa08b706976de96efd
Philippe Villeneuve <Philippe.Villeneuve@primerogames.com>
2020-10-26 08:38:17 -0400

    Retrieving resources with GET https://youtu.be/0CwGTcrGBwI

--- Tweetbook/Controllers/V1/PostsController.cs
+++ Tweetbook/Controllers/V1/PostsController.cs
@@ -1,30 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using Tweetbook.Contracts.V1;
 using Tweetbook.Contracts.V1.Requests;
 using Tweetbook.Contracts.V1.Responses;
 using Tweetbook.Domain;
+using Tweetbook.Services;
 
 namespace Tweetbook.Controllers.V1
 {
     public class PostsController : Controller
     {
-    private List<Post> _posts;
-        public PostsController()
+        private IPostService _postService;
+
+        public PostsController(IPostService postService)
         {
-            _posts = new List<Post>();
-            for(int i=0; i<5; i++)
-            {
-                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
-            }
+            _postService = postService;
         }
 
         [HttpGet(ApiRoutes.Posts.GetAll)]
         public IActionResult GetAll()
         {
-            return Ok(_posts);
+            return Ok(_postService.GetPosts());
+        }
+
+        [HttpGet(ApiRoutes.Posts.Get)]
+        public IActionResult Get([FromRoute] Guid postId)
+        {
+            var post = _postService.GetPostById(postId);
+
+            if (post == null)
+                return NotFound();
+
+            return Ok(post);
         }
 
         [HttpPost(ApiRoutes.Posts.Create)]
@@ -32,13 +42,13 @@ namespace Tweetbook.Controllers.V1
         {
             var post = new Post { Id = postRequest.Id };
 
-            if (string.IsNullOrEmpty(post.Id))
-                post.Id = Guid.NewGuid().ToString();
+            if (Guid.Empty == post.Id)
+                post.Id = Guid.NewGuid();
 
-            _posts.Add(post);
+            _postService.GetPosts().Add(post);
 
             var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
-            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id.ToString());
 
             var response = new PostResponse { Id = post.Id };
             return Created(locationUri, response);
---
+++ Tweetbook/Services/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tweetbook.Domain;
+
+namespace Tweetbook.Services
+{
+    public interface IPostService
+    {
+        List<Post> GetPosts();
+
+        Post GetPostById(Guid postId);
+    }
+}
