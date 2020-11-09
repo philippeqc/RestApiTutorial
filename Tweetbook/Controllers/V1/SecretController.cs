581a208ad6dd7f3e5836aac8d9933b84be1bf4c5
Philippe Villeneuve <Philippe.Villeneuve@primerogames.com>
2020-11-09 15:36:44 -0500

    Setting up ApiKey-based Authentication https://youtu.be/Zo3T_See7iI

---
+++ Tweetbook/Controllers/V1/SecretController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tweetbook.Filters;
+
+namespace Tweetbook.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I have no secrets");
+        }
+
+    }
+}
