6da2017cdfdb6ee8de4cbc231060a06d4f6330b5
Philippe Villeneuve <Philippe.Villeneuve@primerogames.com>
2020-11-10 16:11:11 -0500

    Implementing pagination (part 3)
    Fixed Integration Tests

--- Tweetbook.IntegrationTests/PostsControllerTests.cs
+++ Tweetbook.IntegrationTests/PostsControllerTests.cs
@@ -1,10 +1,12 @@
 ﻿using FluentAssertions;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Tweetbook.Contracts.V1;
 using Tweetbook.Contracts.V1.Requests;
+using Tweetbook.Contracts.V1.Responses;
 using Tweetbook.Domain;
 using Xunit;
 
@@ -23,7 +25,7 @@ namespace Tweetbook.IntegrationTests
 
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
-            (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+            (await response.Content.ReadAsAsync<PagedResponse<PostResponse>>()).Data.Should().BeEmpty();
         }
 
         [Fact]
@@ -31,16 +33,21 @@ namespace Tweetbook.IntegrationTests
         {
             // Arrange
             await AuthenticateAsync();
-            var createdPost = await CreatePostAsync(new CreatePostRequest {Name = "Test post"});
+            var createdPost = await CreatePostAsync(new CreatePostRequest
+            {
+                Name = "Test post",
+                Tags = new []{ "testtag" }
+            });
 
             // Act
             var response = await TestClient.GetAsync(ApiRoutes.Posts.Get.Replace("{postId}", createdPost.Id.ToString()));
 
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
-            var returnedPost = await response.Content.ReadAsAsync<Post>();
-            returnedPost.Id.Should().Be(createdPost.Id);
-            returnedPost.Name.Should().Be("Test post");
+            var returnedPost = await response.Content.ReadAsAsync<Response<PostResponse>>();
+            returnedPost.Data.Id.Should().Be(createdPost.Id);
+            returnedPost.Data.Name.Should().Be("Test post");
+            returnedPost.Data.Tags.Single().Name.Should().Be("testtag");
         }
     }
 }
